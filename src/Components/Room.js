8ca4b773f1ae43e2f2088318f4e9f9b022cf3c47
tomriddle7 <tomriddle7@naver.com>
2020-01-03 21:40:58 +0900

    Update Room.js

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -4,32 +4,65 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled("div")`
-  margin-bottom: 15px;
-  display: block;
-  margin:auto;
+  width:100%;
+  display: flex;
+  flex-direction: row;
+  background-color: #000000;
 `;
 
-const Image = styled.div`
+const Scope60 = styled("div")`
+  width:60vw;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  font-size:5vw;
+`;
+
+const Scope20 = styled("div")`
+  width:20vw;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  font-size:5vw;
+  color: #0b84fe;
+`;
+
+const Image = styled("div")`
   background-image: url(${props => props.bgUrl});
-  height: 180px;
-  border-radius: 4px;
-  background-position: left;
+  background-position: center;
+  background-size: cover;
+  width: 20vw;
+  height: 20vw;
+  border-radius: 22.37%;
+`;
+
+const Name = styled("span")`
+  color: #ffffff;
+`;
+
+const Genre = styled("span")`
+  color: #808084;
 `;
 
 const Room = ({ id, isGame, name, genresName, url, artworkUrl100 }) => (
-  <Container>
-    <Link to={isGame ? `/game/${id}` : `/application/${id}`}>
-        <Image bgUrl={artworkUrl100} />
-        <div>
-          <h1>{name}</h1>
-          <h1>{genresName}</h1>
-        </div>
-        
-        <a href={url}>
-            <h1>받기</h1>
-        </a>
-    </Link>
-  </Container>
+  <Link to={isGame ? `/game/${id}` : `/application/${id}`}>
+    <Container>
+    <Scope20>
+      <Image bgUrl={artworkUrl100}></Image>
+    </Scope20>
+    <Scope60>
+      <Name>{name}</Name>
+      <Genre>{genresName}</Genre>
+    </Scope60>
+    <Scope20>받기</Scope20>
+    </Container>
+  </Link>
+  
 );
 
 Room.propTypes = {
