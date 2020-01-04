b572ae7f6231d3bf02b8c63cbb1d2e326318be84
tomriddle7 <tomriddle7@naver.com>
2020-01-04 10:44:57 +0900

    Updates

---
+++ precache-manifest.268621b9aaf69cfb3534ea14f2fe7e1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdab355544cf0dda7acd46602e68f5a",
+    "url": "/nomad_store/index.html"
+  },
+  {
+    "revision": "7d2fd705955cda3d0777",
+    "url": "/nomad_store/static/js/2.602c7fe7.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/nomad_store/static/js/2.602c7fe7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "119c8fab9566329137a5",
+    "url": "/nomad_store/static/js/main.b3c86a5b.chunk.js"
+  },
+  {
+    "revision": "e39a1334b5e5a2e19b90",
+    "url": "/nomad_store/static/js/runtime-main.f9e59621.js"
+  }
+]);
