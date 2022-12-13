9435cea17ee014f72d49c0d48aabbd5b9754924a
tomriddle7 <tomriddle7@naver.com>
2022-12-13 12:42:05 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const api = axios.create();
 
 export const storeApi = {
-  getUpcomingApps: () => api.get("api/v1/kr/ios-apps/new-apps-we-love/all/10/explicit.json"),
+  getUpcomingApps: () => api.get("api/v1/kr/apps/new-apps-we-love/all/10/explicit.json"),
   getUpcomingGames: () => api.get("api/v1/kr/ios-apps/new-games-we-love/all/10/explicit.json"),
-  getTopFreeApps: () => api.get("api/v1/kr/ios-apps/top-free/all/25/explicit.json"),
-  getTopPaidApps: () => api.get("api/v1/kr/ios-apps/top-paid/all/25/explicit.json"),
+  getTopFreeApps: () => api.get("api/v2/kr/apps/top-free/25/apps.json"),
+  getTopPaidApps: () => api.get("api/v2/kr/apps/top-paid/25/apps.json"),
   getTopFreeGames: () => api.get("api/v1/kr/ios-apps/top-free/games/25/explicit.json"),
   getTopPaidGames: () => api.get("api/v1/kr/ios-apps/top-paid/games/25/explicit.json"),
   getDetail: id => api.get(`lookup?id=${id}&country=KR`),
